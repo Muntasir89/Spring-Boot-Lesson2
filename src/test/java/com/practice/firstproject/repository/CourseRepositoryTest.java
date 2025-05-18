70232b101c6ec13a5202ec6865e6fd62441f44e7
Muntasir89 <montachirhossen76@gmail.com>
2025-05-18 23:33:09 +0600

    Many to Many mapping

--- src/test/java/com/practice/firstproject/repository/CourseRepositoryTest.java
+++ src/test/java/com/practice/firstproject/repository/CourseRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.practice.firstproject.repository;
 
 import com.practice.firstproject.entity.Course;
+import com.practice.firstproject.entity.Student;
 import com.practice.firstproject.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 
 import java.util.List;
 
@@ -35,6 +40,66 @@ class CourseRepositoryTest {
                 .teacher(teacher)
                 .build();
 
+        courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination(){
+        Pageable firstPageWithThreeRecords = PageRequest.of(0, 3);
+        Pageable secondPageWithTwoRecords = PageRequest.of(1, 2);
+
+        List<Course> courses = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+
+        long totalElements = courseRepository.findAll(firstPageWithThreeRecords).getTotalElements();
+
+        long totalPages = courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+
+        System.out.println("totalElements = "+totalElements);
+        System.out.println("totalPages = "+totalPages);
+        System.out.println("courses = "+courses);
+    }
+
+    @Test
+    public void findAllSorting(){
+        Pageable sortByTitle = PageRequest.of(0, 2, Sort.by("title"));
+
+        Pageable sortByCreditDesc = PageRequest.of(0, 2, Sort.by("credit").descending());
+
+        Pageable sortByTitleAndCreditDesc = PageRequest.of(0, 2, Sort.by("title").descending().and(Sort.by("credit")));
+
+        List<Course> courses = courseRepository.findAll(sortByTitle).getContent();
+
+        System.out.println("courses = "+courses);
+    }
+
+    @Test
+    public void findByTitleContaining(){
+        Pageable firstPageWithTenRecords = PageRequest.of(0, 10);
+
+        List<Course> courses = courseRepository.findByTitleContaining("D", firstPageWithTenRecords).getContent();
+
+        System.out.println("courses = "+courses);
+    }
+
+    @Test
+    public void saveCourseWithStudentAndTeacher(){
+        Teacher teacher = Teacher.builder()
+                .firstName("Walter")
+                .lastName("White")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Jesse")
+                .lastName("Pinkman")
+                .emailId("jesse91@gmail.com")
+                .build();
+
+        Course course = Course.builder()
+                .title("AI")
+                .credit(3)
+                .teacher(teacher)
+                .build();
+        course.addStudent(student);
         courseRepository.save(course);
     }
 }
